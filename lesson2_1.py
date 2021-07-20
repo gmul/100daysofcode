3dd0915fafc69785aa7ced5499d1d5946dea6a59
gmul <greg@krystaltek.com>
2021-07-20 20:40:51 +1000

    day 2 updates

---
+++ lesson2_1.py
@@ -0,0 +1,22 @@
+
+
+
+#print(3 * (3 + 3) / 3 - 3
+
+
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+weight = int(weight)
+height = float(height)
+height2 = height **2
+bmi = weight / height2
+print(int(bmi))
+
+#or chang DT on the fly
+
+#bmi = int(weight) / float(height) ** 2
+#print(int(bmi)))
