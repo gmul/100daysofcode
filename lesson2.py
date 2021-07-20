92ac920ad5b0e55fca353c6448536d135ec31f58
gmul <greg@krystaltek.com>
2021-07-20 15:20:45 +1000

    Data Types work

--- lesson2.py
+++ lesson2.py
@@ -7,6 +7,24 @@ print("Hello"[4])
 
 
 #2
+print("123" + " 345")
+" turns the integers into a string
+displays 123345
+
+print(123 + 345)
+no quotes means the integers are calculated
+displays 468
+
+
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+num1 = int(two_digit_number[0])
+num2 = int(two_digit_number[1])
+print(num1 + num2)
 
 
 
