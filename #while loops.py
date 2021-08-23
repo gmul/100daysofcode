6124d1ddc5bd1a6163f74dded46927b8f68343fc
gmul <greg@krystaltek.com>
2021-08-23 20:32:59 +1000

    while loops

---
+++ #while loops.py
@@ -0,0 +1,8 @@
+#while loops
+
+number = 4
+
+while number < 10:
+    do_something()
+    number += 1
+    print(number
---
+++ whileloops.py
@@ -0,0 +1,12 @@
+#while loops
+
+number = 4
+
+def print():
+    blah
+
+while number < 10:
+    do something()
+
+    number += 1
+    
